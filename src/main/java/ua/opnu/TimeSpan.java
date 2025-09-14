892a47ab3be3b16c0893728449b17cbe3558507a
stepashkajr <sergey.stepanenko.junior@gmail.com>
2025-09-14 17:43:30 +0200

    Update TimeSpan.java

--- src/main/java/ua/opnu/TimeSpan.java
+++ src/main/java/ua/opnu/TimeSpan.java
@@ -1,46 +1,65 @@
 package ua.opnu;
 
-
 public class TimeSpan {
+    private int hours;
+    private int minutes;
 
-    // TODO: add class fields
-
-    TimeSpan(int hours, int minutes) {
-        // TODO: write constructor body
+    public TimeSpan(int hours, int minutes) {
+        if (hours < 0 || minutes < 0) {
+            throw new IllegalArgumentException("Hours and minutes must be non-negative");
+        }
+        this.hours = hours + minutes / 60;
+        this.minutes = minutes % 60;
     }
 
-    int getHours() {
-        return 0;
+    public int getHours() {
+        return hours;
     }
 
-    int getMinutes() {
-        // TODO: write method body
-        return 0;
+    public int getMinutes() {
+        return minutes;
     }
 
-    void add(int hours, int minutes) {
-        // TODO: write method body
+    public void add(int hours, int minutes) {
+        int totalMinutes = this.minutes + minutes;
+        this.hours += hours + totalMinutes / 60;
+        this.minutes = totalMinutes % 60;
     }
 
-    void addTimeSpan(TimeSpan timespan) {
-        // TODO: write method body
+    public void addTimeSpan(TimeSpan timespan) {
+        add(timespan.hours, timespan.minutes);
     }
 
-    double getTotalHours() {
-        // TODO: write method body
-        return 0;
+    public double getTotalHours() {
+        return hours + minutes / 60.0;
     }
 
-    int getTotalMinutes() {
-        // TODO: write method body
-        return 0;
+    public int getTotalMinutes() {
+        return hours * 60 + minutes;
     }
 
-    void subtract(TimeSpan span) {
-        // TODO: write method body
+    public void subtract(TimeSpan span) {
+        int totalMinutes1 = getTotalMinutes();
+        int totalMinutes2 = span.getTotalMinutes();
+        int result = totalMinutes1 - totalMinutes2;
+        if (result < 0) {
+            throw new IllegalArgumentException("Resulting TimeSpan cannot be negative");
+        }
+        this.hours = result / 60;
+        this.minutes = result % 60;
     }
 
-    void scale(int factor) {
-        // TODO: write method body
+    public void scale(int factor) {
+        if (factor < 0) {
+            throw new IllegalArgumentException("Scale factor must be non-negative");
+        }
+        int total = getTotalMinutes() * factor;
+        this.hours = total / 60;
+        this.minutes = total % 60;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d hours %d minutes", hours, minutes);
     }
 }
