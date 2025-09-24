0f29f56a2b2b3c78d865288fed36d9396b612cd9
stepashkajr <sergey.stepanenko.junior@gmail.com>
2025-09-24 19:27:34 +0200

    Update Main.java

--- src/main/java/ua/opnu/Main.java
+++ src/main/java/ua/opnu/Main.java
@@ -2,18 +2,23 @@ package ua.opnu;
 
 public class Main {
     public static void main(String[] args) {
-        
         BankAccount acc = new BankAccount(1000.0, 5.0);
         System.out.println("Balance: " + acc.getBalance());
         System.out.println("Transaction Fee: " + acc.getTransactionFee());
 
-        TimeSpan ts1 = new TimeSpan(2, 30); 
-        TimeSpan ts2 = new TimeSpan(1, 45); 
-        
-        ts1.add(ts2.getHours(), ts2.getMinutes());
+        acc.deposit(200);
+        System.out.println("Balance after deposit: " + acc.getBalance());
+
+        acc.withdraw(150);
+        System.out.println("Balance after withdrawal: " + acc.getBalance());
+
+        TimeSpan ts1 = new TimeSpan(2, 30);
+        TimeSpan ts2 = new TimeSpan(1, 45);
+
+        ts1.add(ts2);
         System.out.println("Sum of time spans: " + ts1);
 
-        ts1.subtract(ts2.getHours(), ts2.getMinutes());
+        ts1.subtract(ts2);
         System.out.println("Difference of time spans: " + ts1);
     }
 }
