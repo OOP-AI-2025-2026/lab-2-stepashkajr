c44448fafc1d4cc0c911fce6aa15fa21854fe7f2
stepashkajr <sergey.stepanenko.junior@gmail.com>
2025-09-24 19:24:10 +0200

    Update Main.java

--- src/main/java/ua/opnu/Main.java
+++ src/main/java/ua/opnu/Main.java
@@ -2,23 +2,18 @@ package ua.opnu;
 
 public class Main {
     public static void main(String[] args) {
-        // Створюємо об'єкт BankAccount
+        
         BankAccount acc = new BankAccount(1000.0, 5.0);
-
-        // Виводимо баланс і комісію
         System.out.println("Balance: " + acc.getBalance());
         System.out.println("Transaction Fee: " + acc.getTransactionFee());
 
-        // Приклад використання TimeSpan
-        TimeSpan ts1 = new TimeSpan(2, 30, 15); // 2 години, 30 хв, 15 сек
-        TimeSpan ts2 = new TimeSpan(1, 45, 50); // 1 година, 45 хв, 50 сек
+        TimeSpan ts1 = new TimeSpan(2, 30); 
+        TimeSpan ts2 = new TimeSpan(1, 45); 
+        
+        ts1.add(ts2.getHours(), ts2.getMinutes());
+        System.out.println("Sum of time spans: " + ts1);
 
-        // Додаємо час
-        TimeSpan sum = ts1.add(ts2);
-        System.out.println("Sum of time spans: " + sum);
-
-        // Віднімаємо час
-        TimeSpan diff = ts1.subtract(ts2);
-        System.out.println("Difference of time spans: " + diff);
+        ts1.subtract(ts2.getHours(), ts2.getMinutes());
+        System.out.println("Difference of time spans: " + ts1);
     }
 }
