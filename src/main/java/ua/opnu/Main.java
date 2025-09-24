ffcd3d447c72e65f04aac102bcbb74d6e0a1721c
stepashkajr <sergey.stepanenko.junior@gmail.com>
2025-09-24 19:09:31 +0200

    Update Main.java

--- src/main/java/ua/opnu/Main.java
+++ src/main/java/ua/opnu/Main.java
@@ -2,5 +2,17 @@ package ua.opnu;
 
 public class Main {
     public static void main(String[] args) {
+        BankAccount acc = new BankAccount("Serhii", 1000.0);
+
+        System.out.println("Власник: " + acc.getOwner());
+        System.out.println("Баланс: " + acc.getBalance());
+
+        acc.deposit(500.0);
+        System.out.println("Після депозиту 500: " + acc.getBalance());
+
+        acc.setTransactionFee(50.0);
+        boolean success = acc.withdraw(200.0);
+        System.out.println("Спроба зняти 200 з комісією 50: " + (success ? "успішно" : "не успішно"));
+        System.out.println("Баланс після зняття: " + acc.getBalance());
     }
 }
