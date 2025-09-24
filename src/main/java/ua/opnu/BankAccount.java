6bd6edd895a9d765ca827da344a52e41e569fe2a
stepashkajr <sergey.stepanenko.junior@gmail.com>
2025-09-24 18:45:40 +0200

    Update BankAccount.java

--- src/main/java/ua/opnu/BankAccount.java
+++ src/main/java/ua/opnu/BankAccount.java
@@ -1,27 +1,58 @@
 package ua.opnu;
 
 public class BankAccount {
-    String name;
-    double balance;
-    double transactionFee;
+    private String name;
+    private double balance;
+    private double transactionFee;
 
-    void deposit(double amount) {
-        // TODO: modify method body
-        balance = balance + amount;
+    public BankAccount(String name, double balance) {
+        if (balance < 0) throw new IllegalArgumentException("Initial balance cannot be negative");
+        this.name = name;
+        this.balance = balance;
+        this.transactionFee = 0.0;
     }
 
-    double getBalance() {
-        return this.balance;
+    public String getName() {
+        return name;
     }
 
-    boolean withdraw(double amount) {
-        // TODO: modify method body
-        balance = balance - amount;
-        return true;
+    public double getBalance() {
+        return balance;
     }
 
-    boolean transfer(BankAccount receiver, double amount) {
-        // TODO: modify method body
-        return false;
+    public double getTransactionFee() {
+        return transactionFee;
+    }
+
+    public void setTransactionFee(double fee) {
+        if (fee < 0) throw new IllegalArgumentException("Fee cannot be negative");
+        this.transactionFee = fee;
+    }
+
+    public void deposit(double amount) {
+        if (amount <= 0) return;
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= 0) return;
+        double totalDeduction = amount + transactionFee;
+        if (totalDeduction <= balance) {
+            balance -= totalDeduction;
+        }
+    }
+
+    public void transfer(BankAccount receiver, double amount) {
+        if (receiver == null || amount <= 0) return;
+        double totalDeduction = amount + transactionFee;
+        if (totalDeduction <= balance) {
+            balance -= totalDeduction;
+            receiver.deposit(amount);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name + " : " + balance + " uah";
     }
 }
