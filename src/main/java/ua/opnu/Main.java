e7ace5d2f43e069ea0f6019942fd8a628df892a9
stepashkajr <sergey.stepanenko.junior@gmail.com>
2025-12-02 11:35:51 +0100

    Update Main.java

--- src/main/java/ua/opnu/Main.java
+++ src/main/java/ua/opnu/Main.java
@@ -2,25 +2,15 @@ package ua.opnu;
 
 public class Main {
     public static void main(String[] args) {
+        Student s = new Student("Alice", 3);
+        s.addCourse("Biology");
+        s.addCourse("Chemistry");
+        s.addCourse("Computer Science");
+        s.addCourse("English");
 
-        BankAccount acc = new BankAccount(1000.0, 5.0);
-
-        System.out.println("Balance: " + acc.getBalance());
-        System.out.println("Transaction Fee: " + acc.getTransactionFee());
-
-        acc.deposit(200);
-        System.out.println("Balance after deposit: " + acc.getBalance());
-
-        acc.withdraw(150);
-        System.out.println("Balance after withdrawal: " + acc.getBalance());
-
-        TimeSpan ts1 = new TimeSpan(2, 30);
-        TimeSpan ts2 = new TimeSpan(1, 45);
-
-        ts1.add(ts2);
-        System.out.println("Sum of time spans: " + ts1);
-
-        ts1.subtract(ts2);
-        System.out.println("Difference of time spans: " + ts1);
+        System.out.println("Student: " + s.getName());
+        System.out.println("Number of courses: " + s.getCourseCount());
+        System.out.println("Year of study: " + s.getYear());
+        System.out.println("Tuition paid: " + s.getTuition() + " UAH");
     }
 }
