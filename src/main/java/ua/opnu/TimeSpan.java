7844422f41668ef4a9129802b2523853f68035c7
stepashkajr <sergey.stepanenko.junior@gmail.com>
2025-12-02 10:57:56 +0100

    Update TimeSpan.java

--- src/main/java/ua/opnu/TimeSpan.java
+++ src/main/java/ua/opnu/TimeSpan.java
@@ -1,32 +1,13 @@
 package ua.opnu;
 
 public class TimeSpan {
+
     private int hours;
     private int minutes;
 
     public TimeSpan(int hours, int minutes) {
         this.hours = hours;
         this.minutes = minutes;
-        normalize();
-    }
-
-    public int getHours() {
-        return hours;
-    }
-
-    public int getMinutes() {
-        return minutes;
-    }
-
-    public void add(TimeSpan other) {
-        this.hours += other.hours;
-        this.minutes += other.minutes;
-        normalize();
-    }
-
-    public void subtract(TimeSpan other) {
-        this.hours -= other.hours;
-        this.minutes -= other.minutes;
         normalize();
     }
 
@@ -40,7 +21,65 @@ public class TimeSpan {
             minutes = 60 - (Math.abs(minutes) % 60);
             if (minutes == 60) minutes = 0;
         }
-        if (hours < 0) hours = 0;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    // --- TESTS REQUIRE THIS ---
+    public void add(TimeSpan other) {
+        this.hours += other.hours;
+        this.minutes += other.minutes;
+        normalize();
+    }
+
+    // --- TESTS REQUIRE THIS ---
+    public void add(int hours, int minutes) {
+        this.hours += hours;
+        this.minutes += minutes;
+        normalize();
+    }
+
+    // --- TEST EXPECTS THIS NAME ---
+    public void addTimeSpan(TimeSpan span) {
+        add(span);
+    }
+
+    // --- TESTS REQUIRE THIS ---
+    public void subtract(TimeSpan other) {
+        this.hours -= other.hours;
+        this.minutes -= other.minutes;
+        normalize();
+    }
+
+    // --- TESTS REQUIRE THIS ---
+    public void subtract(int hours, int minutes) {
+        this.hours -= hours;
+        this.minutes -= minutes;
+        normalize();
+    }
+
+    // --- TESTS REQUIRE ---
+    public double getTotalHours() {
+        return hours + (minutes / 60.0);
+    }
+
+    // --- TESTS REQUIRE ---
+    public int getTotalMinutes() {
+        return hours * 60 + minutes;
+    }
+
+    // --- TESTS REQUIRE ---
+    public void scale(int factor) {
+        int total = getTotalMinutes() * factor;
+        this.hours = total / 60;
+        this.minutes = total % 60;
+        normalize();
     }
 
     @Override
